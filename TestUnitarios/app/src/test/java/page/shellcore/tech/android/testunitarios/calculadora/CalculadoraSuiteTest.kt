923748d487e874561b503404e3e21de92ece6dc6
Cesar Morales Garduño <shell.one.core@gmail.com>
2020-04-26 15:15:09 -0500

    Se creó ejemplo de Suite de pruebas.

---
+++ TestUnitarios/app/src/test/java/page/shellcore/tech/android/testunitarios/calculadora/CalculadoraSuiteTest.kt
@@ -0,0 +1,13 @@
+package page.shellcore.tech.android.testunitarios.calculadora
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+@RunWith(value = Suite::class)
+@Suite.SuiteClasses(
+    CalculadoraParametrizadaTest::class,
+    CalculadoraTest::class
+)
+class CalculadoraSuiteTest {
+
+}
